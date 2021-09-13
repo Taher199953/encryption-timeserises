394a68fdcf2e4bdb429b042e7acdee1bb03536ba
Taher199953 <taher.ali@octalinfosolution.com>
2021-09-13 11:30:56 +0530

    changes done

--- index.js
+++ index.js
@@ -6,21 +6,18 @@ const ObjectId = require('mongodb').ObjectId;
 const crypto = require('crypto');
 const path = require('path');
 const WebSocketServer = require('websocket').server;
-
+require('dotenv').config(); 
 const { decrypt } = require('./utils/helpers');
 const { readFile } = require('./utils/filesys');
-
+const { DBUSER,DBPASS } = process.env;
 const app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'view'));
 app.set('view engine', 'ejs');
-
 const server=app.listen(3000, ()=>{console.log("server is listining on port 3000")});
 const io=require('socket.io')(server)
 
 var dbName = "syook",arr=[];
-var client =  new MongoClient(`mongodb+srv://chakkiwala53:chakkiwala53@cluster0-pg0ue.mongodb.net/${dbName}?retryWrites=true&w=majority`,{useNewUrlParser:true,useUnifiedTopology:true});
+var client =  new MongoClient(`mongodb+srv://${DBUSER}:${DBPASS}@cluster0-pg0ue.mongodb.net/${dbName}?retryWrites=true&w=majority`,{useNewUrlParser:true,useUnifiedTopology:true});
 
 var connection;
 client.connect((err, con)=>{
@@ -42,26 +39,27 @@ const wsServer = new WebSocketServer({
 wsServer.on('request', function(request) {
     const connection = request.accept(null, request.origin);
     connection.on('message', function(message) {
-      console.log('Received Message:', message.utf8Data);
-      let obj ={
-          name:"Tanmay Bhatt"
-      }
-    //   connection.sendUTF(JSON.stringify(obj));
+      console.log('Received Data:', message.utf8Data);
 
     });
     connection.on('close', function(reasonCode, description) {
         console.log('Client has disconnected.');
     });
-    // connection.emit
 });
 
 function renderingFile(req,res){
-    // console.log(req.body)
     res.render(path.join(__dirname+'/view/index'))
 
 }
 
 app.get('/',renderingFile);
+
+
+
+let interval = setInterval(async function() {
+    let result = await readFile("data.json");
+    eventEmitter.emit('encrypedData',result);
+}, 10000 );
 
 eventEmitter.on('encrypedData', async (data) => {
 
@@ -90,13 +88,6 @@ eventEmitter.on('encrypedData', async (data) => {
         wsServer.broadcastUTF(JSON.stringify(arr))
     }
 });
-
-
-let interval = setInterval(async function() {
-    let result = await readFile("data.json");
-    eventEmitter.emit('encrypedData',result);
-}, 10000 );
-
 // listen to the event
 
 setInterval(() => {
@@ -105,16 +96,15 @@ setInterval(() => {
     }
     var collection=connection.db(dbName).collection('person');
 
-//   collection.insert(stream,(err,result)=>{
+    collection.insert(stream,(err,result)=>{
 
-//       if(!err){
-//             console.log("data successfully inserted")
-//       }else{
-//               console.log("error occured")
-//       }
-//   })
+        if(!err){
+                console.log("data successfully inserted")
+        }else{
+                console.log("error occured")
+        }
+    })
     arr=[];
-    // clearInterval(interval)
 
 }, 60000);
 
